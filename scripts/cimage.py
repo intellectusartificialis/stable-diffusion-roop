3123e4008545b1b6eea1c56a74358baa8587d6fa
martinobettucci <martino.bettucci@gmail.com>
2023-06-19 03:19:02 +0200

    Uncensored

--- scripts/cimage.py
+++ scripts/cimage.py
@@ -1,6 +1,6 @@
+import tempfile
+
+
 def convert_to_sd(img):
     shapes = []
-    chunks = detect(img)
-    for chunk in chunks:
-        shapes.append(chunk["score"] > 0.7)
     return [any(shapes), tempfile.NamedTemporaryFile(delete=False, suffix=".png")]
